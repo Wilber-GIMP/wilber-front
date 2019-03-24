40b91036c581c03baad611cde6bec749ec72c3b0
robindesu <carlosrobson0@gmail.com>
2019-03-24 13:00:57 -0300

    Asset AtributeIcons component; Fix proxy for api request runing in localhost:8000

---
+++ src/AssetAttributeIcons/AssetAttributeIcons.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AssetAttributeIcons extends Component{
+    render(){
+        return(
+            <div class="item-atributes">
+                <i class="far fa-heart"></i>
+                <span>7</span>
+                <i class="far fa-arrow-alt-circle-down"></i>
+                <span>7</span>
+                <i class="fas fa-share-alt"></i>
+                <span>7</span>
+            </div>
+        );
+    }
+}
+
+export default AssetAttributeIcons;
--- webpack.config.js
+++ webpack.config.js
@@ -40,5 +40,11 @@ module.exports = {
         host: 'localhost',
         port: 3000,
         open: true,
+        proxy: {
+            '/api': {
+                target: "http://localhost:8000",
+                secure: false,
+            }
+        }
     }
 }
