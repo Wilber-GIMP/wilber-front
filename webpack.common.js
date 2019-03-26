30cbcbe56d848c639000aa319c5cd278e1168c32
robindesu <carlosrobson0@gmail.com>
2019-03-26 12:53:46 -0300

    Devide webpack config between dev and prod files; Proxy /media to localhost:8000 where django is runnning

--- webpack.common.js
+++ webpack.common.js
@@ -36,15 +36,4 @@ module.exports = {
         }),
         new CleanWebPackPlugin(),
     ],
-    devServer: {
-        host: 'localhost',
-        port: 3000,
-        open: true,
-        proxy: {
-            '/api': {
-                target: "http://localhost:8000",
-                secure: false,
-            }
-        }
-    }
 }
