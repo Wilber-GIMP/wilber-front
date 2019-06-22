157318c542a145a3c0662f5cfad333dd949bd110
carlos rbson <carlosrobson0@gmail.com>
2019-06-22 13:34:24 -0300

    Organize scss file imports, delete uneecessary file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.scss';
-import './variables.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/App.scss';
+import Home from './Home/Home';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Home />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import * as serviceWorker from './serviceWorker';
+// serviceWorker.unregister();
