e665ee9d117ddd1ff98222d04be921ec2ae4c434
carlos rbson <carlosrobson0@gmail.com>
2019-07-03 18:01:38 -0300

    Handling login inputs

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -2,17 +2,36 @@ import React, {Component} from 'react';
 import './LoginPage.scss';
 
 export class LoginPage extends Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChanges = this.handleChanges.bind(this);
+        this.state ={
+            username : "",
+            pass : ""
+        }
+    }
+
+    handleChanges(event){
+        this.setState({username: event.target.value })
+    };
+
+    handleSubmit(event) {
+        console.log(this.state.username);
+        event.preventDefault();
+    };
+
     render(){
         return(
             <div className="LoginPage">
                 <div className="LoginCard">
                     <h3>Login</h3>
-                    <form>
-                        <label>Email</label>
-                        <input type='email'/>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Username</label>
+                        <input type='text' value={this.state.username} onChange={this.handleChanges}/>
                         <label>Password</label>
-                        <input type='password'/>
-                        <button className="btn-main">SUBMIT</button>
+                        <input type='password' />
+                        <button className="btn-main" type='submit' value='submit'>SUBMIT</button>
                     </form>
                 </div>
             </div>
