2e4e7a3961d30e165065af92b2a01a1f35ccb932
carlos rbson <carlosrobson0@gmail.com>
2019-07-04 19:36:48 -0300

    Validate login form function; enable arrow function as class property  on babel

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,29 +1,39 @@
 import React, {Component} from 'react';
 import './LoginPage.scss';
+import axios from "axios";
 
 export class LoginPage extends Component{
     constructor(props){
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChanges = this.handleChanges.bind(this);
         this.state ={
             username : "",
             pass : ""
         }
     }
 
-    handleChanges(event){
+    handleChanges = (event) => {
         if(event.target.type == 'text'){
             this.setState({username: event.target.value })
         }else{
             this.setState({pass: event.target.value})
         }
     };
-
-    handleSubmit(event) {
+    
+    validateForm = () => {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
         console.log(this.state.username);
         console.log(this.state.pass);
-        event.preventDefault();
+        axios.post("/rest-auth/login/?format=json", {
+           'username': this.state.username,
+           'password': this.setState.pass
+
+        }).then(response => {
+            console.log(response);
+        });
     };
 
     render(){
@@ -36,7 +46,7 @@ export class LoginPage extends Component{
                         <input type='text' value={this.state.username} onChange={this.handleChanges}/>
                         <label>Password</label>
                         <input type='password' value={this.state.pass}  onChange={this.handleChanges}/>
-                        <button className="btn-main" type='submit' value='submit'>SUBMIT</button>
+                        <button className="btn-main" type='submit' value='submit' onClick={this.handleSubmit}>SUBMIT</button>
                     </form>
                 </div>
             </div>
--- webpack.dev.js
+++ webpack.dev.js
@@ -16,6 +16,10 @@ module.exports = merge(common, {
             '/media': {
                 target: "http://localhost:8000",
                 secure: false,
+            },
+            '/rest-auth': {
+                target: "http://localhost:8000",
+                secure: false,
             }
         }
     }
