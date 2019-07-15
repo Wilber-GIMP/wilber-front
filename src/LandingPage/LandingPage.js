9f5a9087934c0d74e9a2be65b100c17f358971b8
carlos rbson <carlosrobson0@gmail.com>
2019-07-15 15:44:24 -0300

    basic landing text and buttons

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,9 +4,21 @@ import logo from './logo.png';
 
 function Landing(props){
     return(
-        <div className='landinng-content'>
+        <div className='landing-content'>
             <img src={logo} id='logo-landing' ></img>
             <h1>Wilber Social</h1>
+            <h3>Share and Download Grafic Assets</h3>
+            <h5>You can can find GIMP Bruches, Color Palletsand many more. </h5>
+            <div className='landing-options'>
+                <div className='download-section'>
+                    Download the Plugin for GIMP to upload or download the Assets
+                    <button className='btn-main'>DOWNLOAD PLUGIN</button>
+                </div>
+                <div className='social-section'>
+                    Access our site to download the Availables Assets
+                    <button className='btn-main'>ACCESS SITE</button>
+                </div>
+            </div>
         </div>
     );
 }
