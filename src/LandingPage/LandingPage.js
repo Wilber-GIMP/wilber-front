7856027df4ab6a6a68d406340fce0884bfe8e958
carlos rbson <carlosrobson0@gmail.com>
2019-11-02 21:22:03 -0300

    Logo in SVG

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import './LandingPage.scss'
 function Landing(props){
     return(
         <div className='landing-content'>
-            <img src='/static/logo_300.png' id='logo-landing' ></img>
+            <img src='/static/images/logo-wilber.svg' id='logo-landing' ></img>
             <h1>Wilber Social</h1>
             <h3>Share and Download Graphic Assets<br></br>
             You can find GIMP Bruches, Color Pallets and many more. </h3>
