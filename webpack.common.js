c7ca1e6664688afd810e85f60ed53d667d778d3d
carlos rbson <carlosrobson0@gmail.com>
2019-12-22 13:54:56 -0300

    Return output to the root public path.

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         filename: '[name].[hash].js',
         path: path.resolve('./dist'),
-        publicPath: "/static/js/"
+        publicPath: "/"
     },
     module:{
         rules:[
