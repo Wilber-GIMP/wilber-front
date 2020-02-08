b1833e0a54633dfca10138ea7cd8acf55d1a937f
carlos rbson <carlosrobson0@gmail.com>
2020-02-08 13:26:47 -0300

    Transform loginpage in functional component and set islogged context.

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,157 +1,134 @@
-import React, {Component} from 'react';
+import React, {Component, useContext, useState} from 'react';
 import './LoginPage.scss';
 import axios from "axios";
 import {Redirect} from 'react-router-dom';
+import LoginContext from '../../states/loginContext';
 
-export class LoginPage extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            email: '',
-            username : '',
-            pass : '',
-            pass2: '',
-            error: '',
-            isRegister: 0,
-            toProfile: false
-        }
-    }
-    //Same handle for Login and Registration
-    handleChanges = (event) => {
-        this.setState({
-            error: false
-        });
-        if(event.target.name == 'username'){
-            this.setState({username: event.target.value });
-        }else if(event.target.name  == 'email'){
-            this.setState({email: event.target.value});
-        }else if(event.target.name == 'password1'){
-            this.setState({pass: event.target.value});
-        }else if(event.target.name == 'password2'){
-            this.setState({pass2: event.target.value});
-        }
-    };
+export const LoginPage = () => {
+            const [email, setEmail] = useState('');
+            const [username, setUsername]  = useState('');
+            const [pass, setPass]  = useState('');
+            const [pass2, setPass2] = useState('');
+            const [error, setError] = useState(false);
+            const [isRegister, setIsregister] = useState('');
+            const [toProfile, setToprofile] = useState(false);
+            const {isLogged,setIslogged } = useContext(LoginContext);
     
-    validateForm = () => {
-        return this.state.username.length > 0 && this.state.pass.length > 0;
+    const validateForm = () => {
+        return username.length > 0 && pass.length > 0;
     };
 
-    saveToken = (response) =>{
+    const saveToken = (response) => {
         if(response.data.key){
             localStorage.setItem('login_token', response.data.key);
+            setToprofile(true);
+            setIslogged(true);
         }
-        this.setState({
-            toProfile: true
-        })
-    };
+    }
 
-    submitLogin = (event) => {
+
+    const submitLogin = (event) => {
         event.preventDefault();
         axios.post("/rest-auth/login/", {
-           'username': this.state.username,
-           'password': this.state.pass
+           'username': username,
+           'password': pass
         }).then(response => {
-            this.saveToken(response);
+            saveToken(response);
         })
         .catch(erro => {
             console.log(erro);
-            this.setState({
-                error: 'User or Password Wrong'
-            })
+            setError('User or Password Wrong');
         });
     };
 
-    submitRegister = (event) =>{
+    const submitRegister = (event) =>{
         event.preventDefault();
         axios.post("/rest-auth/registration/", {
-           'email': this.state.email, 
-           'username': this.state.username,
-           'password1': this.state.pass,
-           'password2': this.state.pass2,
+           'email': email, 
+           'username': username,
+           'password1': pass,
+           'password2': pass2,
         }).then(response => {
-            this.saveToken(response);
+            saveToken(response);
         }).catch(erro => {
             console.log(erro);         
             if(erro.response.data.email){
-                this.setState({
+                setState({
                     error: erro.response.data.email.join("\n")
                 }) 
             }else if(erro.response.data.username){
-                this.setState({
+                setState({
                     error: erro.response.data.username.join("\n")
             }) 
             }else if(erro.response.data.password1){                
-                this.setState({
+                setState({
                     error: erro.response.data.password1.join("\n")
                 })
             }else if(erro.response.data.password2){
-                this.setState({
+                setState({
                     error: erro.response.data.password2.join("\n")
                 })
             }else if(erro.response.data.non_field_errors){
-                this.setState({
+                setState({
                     error: erro.response.data.non_field_errors.join("\n")
                 })
             }else{
-                this.setState({
+                setState({
                     error: 'Unable to Register, check your data and try again.'
                 })
             }
         });
     }
 
-    changeLogin = () => {
-        this.setState({
-            isRegister: !this.state.isRegister
-        })
+    const changeLogin = () => {
+        setIsregister(!isRegister);
     }
 
-    render(){
         let form;
-        if(this.state.toProfile){
-            return <Redirect to={'/profile/'+ this.state.username}/>
+        if(toProfile){
+            return <Redirect to={'/profile/'+ username}/>
         }
-        if(this.state.isRegister) {
+        if(isRegister) {
             form =  
-            <form onSubmit={this.submitRegister}>
+            <form onSubmit={submitRegister}>
                 <label>Email</label>
-                <input name='email' type='email' value={this.state.email} onChange={this.handleChanges}/>
+                <input name='email' type='email' value={email} onChange={event => setEmail(event.target.value)}/>
                 <label>Username</label>
-                <input name='username' type='text' value={this.state.username} onChange={this.handleChanges}/>
+                <input name='username' type='text' value={username} onChange={event => setUsername(event.target.value)}/>
                 <label>Password</label>
-                <input name='password1' type='password' value={this.state.pass}  onChange={this.handleChanges}/>
+                <input name='password1' type='password' value={pass}  onChange={event => setPass(event.target.value)}/>
                 <label>Repeat Password</label>
-                <input name='password2' type='password' value={this.state.pass2}  onChange={this.handleChanges}/>
-                <span className={this.state.error? 'errormsg' : 'noerror'}>{this.state.error}</span>
+                <input name='password2' type='password' value={pass2}  onChange={event => setPass2(event.target.value)}/>
+                <span className={error? 'errormsg' : 'noerror'}>{error}</span>
                 <button className="btn-main" 
                     type='submit' value='submit' 
-                    onClick={this.submitRegister}
-                    disabled={!this.validateForm()}>SUBMIT</button>
+                    onClick={submitRegister}
+                    disabled={!validateForm()}>SUBMIT</button>
             </form>
         }else{
             form =  
-            <form onSubmit={this.submitLogin}>
+            <form onSubmit={submitLogin}>
                 <label>Username</label>
-                <input name='username' type='text' value={this.state.username} onChange={this.handleChanges}/>
+                <input name='username' type='text' value={username} onChange={event => setUsername(event.target.value)}/>
                 <label>Password</label>
-                <input name='password1' type='password' value={this.state.pass}  onChange={this.handleChanges}/>
-                <span className={this.state.error? 'errormsg' : 'noerror'}>{this.state.error}</span>
+                <input name='password1' type='password' value={pass}  onChange={event => setPass(event.target.value)}/>
+                <span className={error? 'errormsg' : 'noerror'}>{error}</span>
                 <button className="btn-main" 
                     type='submit' value='submit' 
-                    onClick={this.submitLogin}
-                    disabled={!this.validateForm()}>SUBMIT</button>
+                    onClick={submitLogin}
+                    disabled={!validateForm()}>SUBMIT</button>
             </form>
         }
         return(
             <div className="LoginPage">
                 <div className="LoginCard">
                     <div id='logintype'>
-                        <button className={'log-btn' + (!this.state.isRegister ? ' active' : '')} 
-                            onClick={this.changeLogin}
-                            disabled={!this.state.isRegister}>Login</button>
-                        <button className={'log-btn' + (this.state.isRegister ? ' active' : '')} 
-                            onClick={this.changeLogin}
-                            disabled={this.state.isRegister}>Register</button>
+                        <button className={'log-btn' + (!isRegister ? ' active' : '')} 
+                            onClick={changeLogin}
+                            disabled={!isRegister}>Login</button>
+                        <button className={'log-btn' + (isRegister ? ' active' : '')} 
+                            onClick={changeLogin}
+                            disabled={isRegister}>Register</button>
                     </div>
                     {form}
                         <a title="Google" className="socialaccount_provider google" href="/accounts/google/login/?process=login">Google Login</a>
@@ -159,7 +136,5 @@ export class LoginPage extends Component{
             </div>
         );
     }
-
-}
 
 export default LoginPage;
